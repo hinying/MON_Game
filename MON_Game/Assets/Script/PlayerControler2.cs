aafeda2a93cfbf9d7af9cff9633267fd657aa5d3
hinying <lisawu3032117@gmail.com>
2022-11-14 13:41:59 +0800

    add object

---
+++ MON_Game/Assets/Script/PlayerControler2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace HSIN
+{
+    /// <summary>
+    /// 左右控制
+    /// </summary>
+    public class PlayerControler2 : MonoBehaviour
+    {
+        public float speed = 20;
+        public float Hinput;
+
+        private void Update()
+        {
+            Hinput = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * Time.deltaTime * Hinput * speed);
+        }
+    }
+}
